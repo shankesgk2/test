89a97c3375545512d140e721ec75753593e799e2
ds <sgk@dsdeiMac.local>
2017-11-16 16:53:15 +0800

    +

--- src/app/routes/pages/login/login.component.ts
+++ src/app/routes/pages/login/login.component.ts
@@ -4,27 +4,28 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { SettingsService } from '@core/services/settings.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-
+import {NzMessageService} from 'ng-zorro-antd';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/catch';
 @Component({
   selector: 'app-pages-login',
   templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
-  public jwtHelper: JwtHelperService;
   valForm: FormGroup;
+  loginLoadding: boolean;
 
-  constructor(public settings: SettingsService, private http: HttpClient, fb: FormBuilder, private router: Router) {
+  constructor(public settings: SettingsService, public _message: NzMessageService, public jwtHelper: JwtHelperService, private http: HttpClient, fb: FormBuilder, private router: Router) {
+    this.loginLoadding = false;
     this.valForm = fb.group({
       email: [null, Validators.compose([Validators.required, Validators.email])],
       password: [null, Validators.required],
       remember_me: [null]
     });
   }
-  // tslint:disable-next-line:one-line
   ngOnInit(): void {
-    console.log(this.jwtHelper);
     const token = this.jwtHelper.tokenGetter();
-    // tslint:disable-next-line:one-line
     if (token != null) {
       if (!this.jwtHelper.isTokenExpired(token)) {
         this.router.navigate(['/']);
@@ -32,24 +33,29 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  submit() {
-    // tslint:disable-next-line:forin
-    for (const i in this.valForm.controls) {
-      this.valForm.controls[i].markAsDirty();
-    }
+  submit(): Promise<any> {
     if (this.valForm.valid) {
-      console.log('Valid!');
-      console.log(this.valForm.value);
       const value = {
         username: this.valForm.value.email,
         password: this.valForm.value.password,
         remember_me: [null]
       };
-      this.http.post('login', value).subscribe(data => {
-        console.log(data['status_code']);
-        console.log(data);
-        localStorage.setItem('token', data['token']);
-        this.router.navigate(['dashboard']);
+      this.loginLoadding = true;
+      return new Promise((resolve, reject) => {
+        this.http.post('login', value)
+          .subscribe((res: any) => {
+            localStorage.setItem('token', res.token);
+            this.router.navigate(['dashboard']);
+            resolve(res);
+          }, (err: HttpErrorResponse) => {
+            this.loginLoadding = false;
+            if (err.error instanceof Error) {
+              this._message.create('error', err.error.message);
+            } else {
+              this._message.create('error', '用户名或密码错误，登录失败');
+            }            
+            resolve(null);
+          });
       });
     }
   }
